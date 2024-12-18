1dbddd91fc882a09d061662ee4aedbd5d37fb742
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2024-12-18 18:28:19 +0300

    Working on convert feature

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const addButton = document.getElementById("add")
+const converterButton = document.getElementById("convertButton")
 const viewer = document.getElementById("displayer")
+const rowCounterDiv = document.getElementById("rowCounterDiv")
+const noneDiv = document.getElementById("noneDiv")
+const dateInput = document.getElementById("dateInput")
+const weekCounter=document.getElementById("weekCounter")
+const monthCounter=document.getElementById("monthCounter")
+const yearCounter=document.getElementById("yearCounter")
+const inputDiv = document.getElementById("inputDivs")
 var days = 0;
 var week = 0;
 var month = 0;
 var year = 0;
 var checher= 0 ;
-const weekCounter=document.getElementById("weekCounter")
-const monthCounter=document.getElementById("monthCounter")
-const yearCounter=document.getElementById("yearCounter")
 
+weekCounter.classList.remove("remover")
+monthCounter.classList.remove("remover")
+yearCounter.classList.remove("remover")
+viewer.classList.remove("remover")
+rowCounterDiv.classList.remove("remover")
+noneDiv.classList.remove("remover")
 
 
 function counter(){
@@ -39,4 +50,15 @@ function counter(){
     
 
 }
-// console.log(days)
+
+
+function convertButton(){
+  yearCounter.classList.toggle("remover")
+ monthCounter.classList.toggle("remover")
+ weekCounter.classList.toggle("remover")
+ inputDiv.classList.toggle("remover")
+ viewer.classList.toggle("remover")
+ rowCounterDiv.classList.toggle("remover")
+ noneDiv.classList.toggle("remover")
+ 
+}
